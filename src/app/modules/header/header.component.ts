a0675567a4467bde685178bed4f473a6ce80a18c
Pawan Das <pawandas14@gmail.com>
2023-07-13 14:09:48 +0530

    first commit

---
+++ src/app/modules/header/header.component.ts
@@ -0,0 +1,16 @@
+import { DatePipe } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.css'],
+  providers: [DatePipe]
+})
+export class HeaderComponent implements OnInit {
+
+  constructor() { }
+  ngOnInit() {
+  }
+
+}
